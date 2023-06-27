7dd1fb7555fcfa74916ef4935797ada111b71cb0
mncosta <migascosta@hotmail.com>
2023-06-27 15:32:05 +0100

    README & Code

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# aet_cet
+# Cycling Safety Data Augmentation in the Urban Environment
 Accident Environment Types &amp; City Environment Types
+
+
+This code explores the use of Spectral Clustering of network infrastructure data (circulation spaces from OpenStreetMap) to define Accident Environment Types (AET) and City Environment Types (CET). AET and CET can be compared to explore differences between built environment classes where cycling accidents occured versus city-wide common environments. Here, we apply this methodology to compare AET and CET from Barcelona, Spain, and New York, USA.
+
+<table>
+<tr>
+<th> Good </th>
+<th> Bad </th>
+</tr>
+<tr>
+<td>
+
+<img title="AET in Barcelona" alt="AET" src="/images/clustering_sp_barcelona_25.png">
+
+</td>
+<td>
+
+<img title="CET in Barcelona" alt="CET" src="/images/clustering_sp_barcelonaRS_25.png">
+
+</td>
+</tr>
+</table>
+
+
+
+### License
+
+Our code is under [MIT license](LICENSE).
+
+
+
+### Citation
+
+If you find this project useful for your research, please use the following BibTeX entry.
+
+
+```
+@article{costa2022augmenting,
+  title={Cycling Safety Data Augmentation in the Urban Environment: A Barcelona Case Study},
+  author={Costa, Miguel and Roque, Carlos and Marques, Manuel and Moura, Filipe}
+  journal={International Cycling Safety Conference 2022},
+  year={2022},
+}
+```
---
+++ plot_init.py
@@ -0,0 +1,35 @@
+import matplotlib
+
+from matplotlib.backends.backend_pgf import FigureCanvasPgf
+
+
+MPL_RCPARAMS = {
+    "figure.autolayout": True,     # Makes sure the figure is neat & tight.
+    "figure.figsize": (5.78, 3.0), # textwidth = 146.8mm, c.f. preamble.tex.
+    "figure.dpi": 150,             # Displays figures nicely in notebooks.
+    "axes.linewidth": 0.5,         # Matplotlib's current default is 0.8.
+    "lines.linewidth": 1.0,
+    "lines.markersize" : 4,
+    "xtick.major.width": 0.5,
+    "xtick.minor.width": 0.5,
+    "xtick.labelsize": 11,
+    "xtick.top": True,
+    "ytick.major.width": 0.5,
+    "ytick.minor.width": 0.5,
+    "ytick.labelsize": 11,
+    "ytick.right": True,
+    "text.usetex": True,           # use LaTeX to write all text
+    "font.family": "serif",        # use serif rather than sans-serif
+    "font.serif": "lmodern",
+    "font.size": 11,
+    "axes.titlesize": 11,          # LaTeX default is 10pt font.
+    "axes.labelsize": 11,           # LaTeX default is 10pt font.
+    "legend.fontsize": 9,          # Make the legend/label fonts a little smaller
+    "legend.frameon": False,       # Remove the black frame around the legend
+}
+
+
+def setup_plotting():
+    matplotlib.backend_bases.register_backend('pdf', FigureCanvasPgf)
+    matplotlib.rcParams.update(MPL_RCPARAMS)
+    print("Plotting settings loaded!")
